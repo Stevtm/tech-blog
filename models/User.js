57cf3d6d23e07554407e871f1db4f5ea658ea46e
Stevtm <steventmaddison@outlook.com>
2021-05-27 08:16:35 -0400

    create User model

---
+++ models/User.js
@@ -0,0 +1,63 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+const bcrypt = require("bcrypt");
+
+// create User model
+class User extends Model {
+	// add password veridication function call here
+}
+
+// define the table columns and configurations
+User.init(
+	{
+		// table columns definitions
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		username: {
+			type: DataTypes.STRING(30),
+			allowNull: false,
+		},
+		email: {
+			type: DataTypes.STRING(30),
+			allowNull: false,
+			unique: true,
+			validate: {
+				isEmail: true,
+			},
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+	},
+	{
+		// set up hooks for data manipulation
+		hooks: {
+			// hash password on User creation
+			async beforeCreate(newUserData) {
+				newUserData.password = await bcrypt.hash(newUserData.password, 10);
+				return newUserData;
+			},
+			// hash password on User update
+			async beforeUpdate(updatedUserData) {
+				updatedUserData.password = await bcrypt.hash(
+					updatedUserData.password,
+					10
+				);
+				return updatedUserData;
+			},
+		},
+		// table configuration options
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: "user",
+	}
+);
+
+module.exports = User;
