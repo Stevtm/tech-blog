47e133fd6c1ec66a66d02bbc4f47e40c746ea5e9
Stevtm <steventmaddison@outlook.com>
2021-05-29 00:44:47 -0400

    create (unstyled) signup page in handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,9 +2,14 @@ const router = require("express").Router();
 const sequelize = require("../config/connection");
 const { User, Post, Comment } = require("../models");
 
-// show login and signup page
+// show login page
 router.get("/login", (req, res) => {
 	res.render("login");
+});
+
+// show signup page
+router.get("/signup", (req, res) => {
+	res.render("signup");
 });
 
 module.exports = router;
